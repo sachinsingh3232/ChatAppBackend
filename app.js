cc326868816c03b3f95d7f3438a58b872ce79e88
SACHIN SINGH <92503784+sachinsingh3232@users.noreply.github.com>
2023-06-30 22:55:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const chatRoute = require('./Routes/chatRoute')
 const messageRoute = require('./Routes/messageRoute')
 const {notFound,errorHandler}=require('./Middleware/errorMiddleware')
 const cors = require('cors')
-app.use(cors())
-// {
-//     origin: process.env.CORS_URL,
-//     credentials: true
-// }
+app.use(cors(
+  {
+    origin: process.env.CORS_URL,
+    credentials: true
+}
+))
 app.use(express.json())
 app.use("/chatApp/user", userRoute)
 app.use("/chatApp/chat", chatRoute)
