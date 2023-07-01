be85e31c7d0776bf5d6c5dbc429e99e03af25fef
SACHIN SINGH <92503784+sachinsingh3232@users.noreply.github.com>
2023-07-01 14:24:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const server = app.listen(PORT, () => {
 const io = require('socket.io')(server, {
     pingTimeout: 60000,
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://chat-app-frontend-omega.vercel.app",
     },
 });
 io.on("connection", (socket) => {
